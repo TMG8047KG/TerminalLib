3bb6d2a4ce1d7dcfe9461012be0eff2afd6d7c92
TMG <66220144+TMG8047KG@users.noreply.github.com>
2023-04-18 00:04:40 +0300

    1.0.2-beta documentation

--- src/Core/TerminalScheduleUpdate.java
+++ src/Core/TerminalScheduleUpdate.java
@@ -1,5 +1,17 @@
 package Core;
 
+/**
+ * Executes code periodically
+ *
+ * @author TMG8047KG
+ * @version 1.0.2-beta
+ * */
 public interface TerminalScheduleUpdate {
+    /**
+     * This is overrideable
+     * <p>
+     *     Everything in this method executes in the set and given delay
+     * </p>
+     * */
     void Update();
 }
